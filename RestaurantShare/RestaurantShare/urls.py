80a5f68802e6a14e1b5aa362450d1b00d27cbb0b
jsj3282 <jeongseonju15@gmail.com>
2022-05-23 22:07:27 +0900

    commit

--- RestaurantShare/RestaurantShare/urls.py
+++ RestaurantShare/RestaurantShare/urls.py
@@ -13,9 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+# from django.conf.urls import url
+# from django.contrib import admin
+
+# urlpatterns = [
+#     url(r'^admin/', admin.site.urls),
+#     url(r'^sendEmail/', sendEmail.urls),
+#     url(r'^shareRes/', shareRes.urls)
+# ]
 from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    path("", include("shareRes.urls")),
+    path("sendEmail/", include("sendEmail.urls")),
+    path("admin/", admin.site.urls),
 ]
