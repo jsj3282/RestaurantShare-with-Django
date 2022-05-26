01c7abae9d369b5d5597a641b09dcc64279cd42c
jsj3282 <jeongseonju15@gmail.com>
2022-05-26 22:28:21 +0900

    commit

--- RestaurantShare/shareRes/urls.py
+++ RestaurantShare/shareRes/urls.py
@@ -3,7 +3,19 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("restaurantDetail/", views.restaurantDetail),
-    path("restaurantCreate/", views.restaurantCreate),
-    path("categoryCreate/", views.categoryCreate),
+    path("restaurantDetail/delete", views.delete_restaurant, name="resDelete"),
+    path("restaurantDetail/<str:res_id>", views.restaurantDetail, name="resDetailPage"),
+    path(
+        "restaurantDetail/updatePage/update", views.update_restaurant, name="resUpdate"
+    ),
+    path(
+        "restaurantDetail/updatePage/<str:res_id>",
+        views.restaurantUpdate,
+        name="resUpdatePage",
+    ),
+    path("restaurantCreate/", views.restaurantCreate, name="resCreatePage"),
+    path("restaurantCreate/create", views.create_restaurant, name="resCreate"),
+    path("categoryCreate/", views.categoryCreate, name="cateCreatePage"),
+    path("categoryCreate/create", views.create_category, name="cateCreate"),
+    path("categoryCreate/delete", views.delete_category, name="cateDelete"),
 ]
